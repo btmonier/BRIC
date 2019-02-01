f766d88bf2f9cf87b8e939f4a6c7d958aec1ea7d
Brandon Monier <brandon.monier@gmail.com>
2019-01-31 19:26:07 -0500

    Optimize pipeline

--- cell_type_prediction/clustering.R
+++ cell_type_prediction/clustering.R
@@ -1,15 +1,15 @@
-setwd("C:/Users/Juan.XIe/Desktop/test1/") # you may change to your own folder
+
 library(igraph)
 library(mclust)
 library(MCL)
 library(clues)
 library(anocva)
 
-RAW <-read.table("Yan_RPKM",header=T,sep="\t")   # expression data
-CellNum <-dim(RAW)[2]-1  # the number of cells 
+RAW <-read.table("example_data/Yan_RPKM",header=T,sep="\t")   # expression data
+CellNum <-dim(RAW)[2]-1  # the number of cells
 
 
-Graph <-read.csv('Yan_RPKM_graph.csv',header=T,sep=",")
+Graph <-read.csv('example_data/Yan_RPKM_graph.csv',header=T,sep=",")
 names(Graph) <-c('Node1','Node2','weight')
 G <-graph.data.frame(Graph,directed = FALSE)  # convert file into graph
 A <- as_adjacency_matrix(G,type="both",attr="weight",names=TRUE,sparse=FALSE)  # convert graph into adjacency matrix
@@ -18,77 +18,77 @@ Covered <-length(V_name)  # the #of covered cells
 
 ## MCL clustering on adjacency matrix
 MCL_cs <-function(A){
-	CLUST <-list()
-	for (i in 1:100){
-		CLUST[[i]] <-mcl(A,addLoops = FALSE,inflation =i,max.iter=200)
-	}
-	KK <- as.data.frame(do.call(rbind,lapply(CLUST,'[[',1)))  # extract the number of clusters
-	CAN_I <-c(which(as.numeric(as.character(KK$V1))>=2)) 	# results that has more than 5 clusters
-	tt <-as.numeric(as.character(KK$V1))
-	tt <-sort(table(tt),decreasing=T)[1]
-	Final_K <-as.numeric(names(tt))
-		
-	if (length(CAN_I)!=0){
-		MATRIX <-rep(0,Covered)%o%rep(0,Covered)
-		for (k in 1:length(CAN_I)){	
-			MCL_label <-CLUST[[CAN_I[k]]]$Cluster  # record the label
-			ClusterNum <-unique(MCL_label)   # record the number of clusters
-			TEMP <-rep(0,Covered)%o%rep(0,Covered)
-			temp <-rep(0,Covered) %o% rep(0,length(ClusterNum))
-			for (n in 1:length(ClusterNum)){
-				index <-which(MCL_label==ClusterNum[n])
-				temp[index,n] <-1
-				TEMP <-TEMP+temp[,n]%o%temp[,n] 
-			}
-		MATRIX <-MATRIX+TEMP
-		}
-		MATRIX <-MATRIX/length(CAN_I)
-		rownames(MATRIX) <-colnames(MATRIX) <-rownames(A)
-		hc <-hclust(dist(MATRIX))
-		memb <-cutree(hc,k=Final_K)
-		if (length(rownames(A)) ==CellNum){
-			label <-memb
-		}else{
-			LEFT <-setdiff(names(RAW)[-1],V_name)
-			LEFT_Cluster <-rep(Final_K+1,length(LEFT))
-			df_cell_label <-data.frame(cell=c(names(memb),LEFT),cluster=c(memb,LEFT_Cluster),K=rep(Final_K+1,CellNum))				
-			label <-df_cell_label$cluster
-		}	
-	}
+    CLUST <-list()
+    for (i in 1:100){
+        CLUST[[i]] <-mcl(A,addLoops = FALSE,inflation =i,max.iter=200)
+    }
+    KK <- as.data.frame(do.call(rbind,lapply(CLUST,'[[',1)))  # extract the number of clusters
+    CAN_I <-c(which(as.numeric(as.character(KK$V1))>=2))     # results that has more than 5 clusters
+    tt <-as.numeric(as.character(KK$V1))
+    tt <-sort(table(tt),decreasing=T)[1]
+    Final_K <-as.numeric(names(tt))
+
+    if (length(CAN_I)!=0){
+        MATRIX <-rep(0,Covered)%o%rep(0,Covered)
+        for (k in 1:length(CAN_I)){
+            MCL_label <-CLUST[[CAN_I[k]]]$Cluster  # record the label
+            ClusterNum <-unique(MCL_label)   # record the number of clusters
+            TEMP <-rep(0,Covered)%o%rep(0,Covered)
+            temp <-rep(0,Covered) %o% rep(0,length(ClusterNum))
+            for (n in 1:length(ClusterNum)){
+                index <-which(MCL_label==ClusterNum[n])
+                temp[index,n] <-1
+                TEMP <-TEMP+temp[,n]%o%temp[,n]
+            }
+        MATRIX <-MATRIX+TEMP
+        }
+        MATRIX <-MATRIX/length(CAN_I)
+        rownames(MATRIX) <-colnames(MATRIX) <-rownames(A)
+        hc <-hclust(dist(MATRIX))
+        memb <-cutree(hc,k=Final_K)
+        if (length(rownames(A)) ==CellNum){
+            label <-memb
+        }else{
+            LEFT <-setdiff(names(RAW)[-1],V_name)
+            LEFT_Cluster <-rep(Final_K+1,length(LEFT))
+            df_cell_label <-data.frame(cell=c(names(memb),LEFT),cluster=c(memb,LEFT_Cluster),K=rep(Final_K+1,CellNum))
+            label <-df_cell_label$cluster
+        }
+    }
 return(label)
 }
 
 ## Spectral clustering on adjacency matrix, need provide the number of clusters
-SC_cs <-function(A,K){   
-	sc <-spectralClustering(A,k=K)
-	names(sc) <-rownames(A)
-	if (length(rownames(A)) ==CellNum){
-		label <-sc
-	}else{
-		LEFT <-setdiff(names(RAW)[-1],V_name)
-		LEFT_Cluster <-rep(K+1,length(LEFT))
-		df_cell_label <-data.frame(cell=c(names(sc),LEFT),cluster=c(sc,LEFT_Cluster),K=rep(K+1,CellNum))				
-		label <-df_cell_label$cluster
-	}
+SC_cs <-function(A,K){
+    sc <-spectralClustering(A,k=K)
+    names(sc) <-rownames(A)
+    if (length(rownames(A)) ==CellNum){
+        label <-sc
+    }else{
+        LEFT <-setdiff(names(RAW)[-1],V_name)
+        LEFT_Cluster <-rep(K+1,length(LEFT))
+        df_cell_label <-data.frame(cell=c(names(sc),LEFT),cluster=c(sc,LEFT_Cluster),K=rep(K+1,CellNum))
+        label <-df_cell_label$cluster
+    }
 return(label)
 }
 
 ## users need to specify which clustering method they want to use
 CLUSTERING <-function(A,K,method){
-	if (method=='MCL_cs'){
-		label <-MCL_cs(A)
-		return(label)
-	}else if (method =='SC_cs'){
-		label <-SC_cs(A,K)
-		return(label)
-	}
+    if (method=='MCL_cs'){
+        label <-MCL_cs(A)
+        return(label)
+    }else if (method =='SC_cs'){
+        label <-SC_cs(A,K)
+        return(label)
+    }
 }
 
-labels <-CLUSTERING(A,'MCL_cs') # get the predicted cell cluster labels
+labels <-CLUSTERING(A = A, method = 'MCL_cs') # get the predicted cell cluster labels
 # or label <-CLUSTERING(A,7,'SC_cs')
 
 ## if you have some reference labels, you can calculate the ARI, RI, FM and JI
-target <-read.table('Yan_cell.csv',header=T,sep=',')
+target <-read.table('example_data/Yan_cell_label.csv',header=T,sep=',')
 head(target)
 # judge if the cell names are consistent
 aa <-names(labels)
@@ -98,13 +98,13 @@ bb <-target$Cell_type
 if (identical(sort(aa),sort(as.character(bb)))=='TRUE'){
   sorted <-labels[match(target$Cell_type,names(labels))] # sort the predicted label
 
-  ARI <-adjustedRandIndex(sorted,target$Cluster)  
+  ARI <-adjustedRandIndex(sorted,target$Cluster)
   RI <-adjustedRand(sorted,target$Cluster,randMethod='Rand')
   FM <-adjustedRand(sorted,target$Cluster,randMethod='FM')
   JI <-adjustedRand(sorted,target$Cluster,randMethod='Jaccard')
   df <-data.frame(ARI=ARI, RandIndex=RI,FolkesMallow=FM, Jaccard=JI)
   df
-  
+
 }else{
   print('Cell names is not consistent, please double check !')
 }
--- cell_type_prediction/constructGraph.R
+++ cell_type_prediction/constructGraph.R
@@ -1,32 +1,32 @@
 ### step2: construct weighted graph  ##
-# setwd('the/path/to/the/blocks/files')  
-F <-readLines('Yan_RPKM.chars.blocks')  # read .blocks file
+# setwd('the/path/to/the/blocks/files')
+F <-readLines('example_data/Yan_RPKM.chars.blocks')  # read .blocks file
 TEMP <-grep('Conds',F,value=T) ## extract condition lines in each BC
 BC <-sapply(strsplit(TEMP,':',2),'[',2) # only keep cell names
 
-CONDS <-as.character()   # store the conditions 
+CONDS <-as.character()   # store the conditions
 label_C <-as.numeric()   # store the occurence of one condistions
 
-for (j in 1:length(BC)){
-	BCcond <-unlist(strsplit(BC[j], split = " "))
-	BCcond <-BCcond[BCcond!=""]  # exclude the blank string
-	CONDS <-c(BCcond,CONDS)
-	label_C <-c(label_C,rep(j,length(BCcond)))
+for (j in seq_len(BC)){
+    BCcond <-unlist(strsplit(BC[j], split = " "))
+    BCcond <-BCcond[BCcond!=""]  # exclude the blank string
+    CONDS <-c(BCcond,CONDS)
+    label_C <-c(label_C,rep(j,length(BCcond)))
 }
 
 df_C <-data.frame(conds=CONDS,label=label_C)
 uniq_C <-df_C$conds[!duplicated(df_C$conds)]   # unique conditions
 Node <-t(combn(uniq_C,2))
-	
+
 Wt <-rep(-1,dim(Node)[1])
 for (k in 1:dim(Node)[1]){
-	member1 <-df_C[which(df_C$conds %in% Node[k,1]),]   # identify which BC the k th Node appear
-	member2 <-df_C[which(df_C$conds %in% Node[k,2]),]
-	Wt[k] <-length(intersect(member1[,2],member2[,2])) # the weight between two node
+    member1 <-df_C[which(df_C$conds %in% Node[k,1]),]   # identify which BC the k th Node appear
+    member2 <-df_C[which(df_C$conds %in% Node[k,2]),]
+    Wt[k] <-length(intersect(member1[,2],member2[,2])) # the weight between two node
 }
 GRAPH <-data.frame(Node[,1],Node[,2],Wt)
-if (dim(GRAPH)[1]!=0)	{
-	write.csv(subset(GRAPH,Wt!=0),"Yan_RPKM_graph.csv",row.names=FALSE)
+if (dim(GRAPH)[1]!=0)    {
+    write.csv(subset(GRAPH,Wt!=0),"example_data/Yan_RPKM_graph.csv",row.names=FALSE)
 }
 
 
